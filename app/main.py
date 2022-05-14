8744e07fefb0d3485f81bf369aebd61818d59003
HydroGest <2445691453@qq.com>
2022-05-14 21:19:32 +0800

    在main.py引入了数据库模块

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 import uvicorn
+import schemas
+import models
 from functools import lru_cache
 from fastapi import FastAPI,Depends
 from config import settings
+from database import engine
 
 app = FastAPI()
+
+models.Base.metadata.create_all(engine)
 
 @app.get("/")
 async def root():
